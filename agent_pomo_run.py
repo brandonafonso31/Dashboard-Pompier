9c2a75b534df7f43fc84a470ca2cd46fda72bab9
AFONSO Brandon <125391799+brandonafonso31@users.noreply.github.com>
2025-07-07 12:35:18 +0200

    add métrique diff par agent

--- agent_pomo_run.py
+++ agent_pomo_run.py
@@ -2,29 +2,28 @@ import subprocess
 import multiprocessing
 from IPython.display import clear_output
 
-def run_agent(agent_id, args_template):
+def run_agent(agent_id):
     """Fonction pour exécuter un agent individuel"""
     
     #print("\nBien arrivé dans la fonction run_agent !")
-    
-    args = args_template.copy()
-    args["--model_name"] = f"{args['--model_name']}_agent_{agent_id}"
-    args["--save_metrics_as"] = f"{args['--save_metrics_as']}_agent_{agent_id}"
+    m = ['z1_sent', 'v_not_found_in_last_station', 'ff_sent', 'ff_skill_mean', 'v_degraded']
+    # , 'function_not_found', 'skill_lvl', 'function_cancelled', 'cancelled', 'v_sent_full', 'v_sent', 'v1_not_sent_from_1st_station']
+    suffix = "r100_cf3"
     
     cmd = [
         "python3", "agent_run.py",
-        "--model_name", args["--model_name"],
-        "--hyper_params", args["--hyper_params"],
-        "--reward_weights", args["--reward_weights"],
-        "--dataset", args["--dataset"],
-        "--start", args["--start"],
-        "--end", args["--end"],
-        "--constraint_factor_veh", args["--constraint_factor_veh"],
-        "--constraint_factor_ff", args["--constraint_factor_ff"],
-        "--save_metrics_as", args["--save_metrics_as"],
-        "--train" if args["--train"] else ""
+        "--model_name", f"pomo_agent_{m[agent_id]}",
+        "--hyper_params", "hyper_params.json",
+        "--reward_weights", f"rw_{m[agent_id]}_{suffix}.json",
+        "--dataset", "df_pc_fake_1y.pkl",
+        "--start", "1",
+        "--end", "53088",
+        "--constraint_factor_veh", "3",
+        "--constraint_factor_ff", "1",
+        "--save_metrics_as", f"agent_pomo_metrics_{m[agent_id]}_{suffix}",
+        "--train"
     ]
-    #print(f"La commande de l'agent {args["--model_name"]} est {cmd}\n")
+    print(f"\nLa commande de l'agent pomo_agent_{m[agent_id]} est {cmd}\n")
     
     process = subprocess.Popen(
         cmd,
@@ -46,26 +45,9 @@ def run_agent(agent_id, args_template):
 if __name__ == "__main__":
     
     #print("\nBien arrivé dans agent_pomo_run !\n")
-    
-    m = 'z1_pomo_sent_'
-    suffix = "r100_cf3"
-    
-    args_template = {
-        "--model_name": f"agent_{m}_{suffix}",
-        "--hyper_params": "hyper_params.json",
-        "--reward_weights": f"rw_z1_sent_{suffix}.json",
-        "--dataset": "df_pc_fake_1y.pkl",
-        "--start": "1",
-        "--end": "53088",
-        "--constraint_factor_veh": "3",
-        "--constraint_factor_ff": "1",
-        "--save_metrics_as": f"agent_pomo_metrics_{m}_{suffix}",
-        "--train": True
-    }
-    
     num_agents = 5  # Nombre d'agents POMO
-    
     print(f"Nombre d'agent = {num_agents}. on démarre le multi process ...")
+    
     # Lancer les agents en parallèle
     with multiprocessing.Pool(processes=num_agents) as pool:
-        pool.starmap(run_agent, [(i, args_template) for i in range(num_agents)])
+        pool.starmap(run_agent, [(i,) for i in range(num_agents)])
