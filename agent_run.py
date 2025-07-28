3af768f14b1a706584fb76782baf781f9305d9f8
Brandon <brandon.afonso3118@gmail.com>
2025-07-28 13:03:56 +0200

    only save on ereward file insteead of more and then replace: now only one reward is save and is load for all agent during the loop

--- agent_run.py
+++ agent_run.py
@@ -638,10 +638,11 @@ if __name__ == "__main__":
 
         os.chdir('../SVG_model')   
         torch.save(agent.qnetwork_local.state_dict(), args.model_name)
-        print("\nAgent saved as", args.model_name, flush=True)        
-        with open(f"../Reward_weights/rw_{args.model_name}_r100_cf3.json", "w") as f:
-            json.dump(dic_indic, f)
-        print(f"Reward : rw_{args.model_name}_r100_cf3.json - updated !\n")
+        print("\nAgent saved as", args.model_name, flush=True) 
+        if args.agent_id == elected:       
+            with open(f"../Reward_weights/rw_{args.model_name}_r100_cf3.json", "w") as f:
+                json.dump(dic_indic, f)
+            print(f"Reward : rw_{args.model_name}_r100_cf3.json - updated !\n")
 
     else:
 
