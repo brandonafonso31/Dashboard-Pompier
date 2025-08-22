4d6eeba0a70d241afc7ae9f52a442dc2e084db82
Brandon <brandon.afonso3118@gmail.com>
2025-08-22 12:04:17 +0200

    regenerate metrics

--- functions_utils_pomo.py
+++ functions_utils_pomo.py
@@ -23,3 +23,16 @@ def get_current_elected(cur_path):
         print("Error reading shared_state.json:", e)
         time.sleep(0.5)
     return -1 
+
+
+metric_list = ["v_required", "v_sent","v_sent_full","v_degraded","cancelled","function_not_found",
+       "v1_not_sent_from_s1","v3_not_sent_from_s3","v_not_found_in_last_station",
+       "ff_required","ff_sent", "rupture_ff","skill_lvl"]
+
+metrics = {}
+i = 0
+for key in metric_list:
+    metrics[str(i)] = key
+    i+=1
+with open(f"Data/metrics.json",'w') as f:
+    json.dump(metrics, f,indent=4)
